484e69d1f5f6d7fa192c7223520a639c6e6668f2
Sajidur Rahman <sajidur.dev@gmail.com>
2026-05-13 04:17:24 +0600

    Banner section done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,17 @@ const Banner = () => {
     <div>
       <div id="banner">
         <div className="container">
-            <div className="banner-text"></div>
+            <div className="banner-area">
+              <div className="banner-text">
+              <h4>Welcome to our CHURCH</h4>
+              <h1>Become a part of our community</h1>
+              <button className='primary-btn'>Learn More</button>
+            </div>
+            <div className="dummy">
+              <div className="line"></div>
+              <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</h5>
+            </div>
+            </div>
         </div>
       </div>
     </div>
